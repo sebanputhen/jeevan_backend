427ead8bbadc0860de6a23cdb1b9e8459a3a10dd
sebanputhen <sebastian.puthenveedu@gmail.com>
2024-12-30 11:31:52 +0530

    render

--- routes/slabRoutes.js
+++ routes/slabRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const slabController1 = require('./controllers/slabController'); 
+const slabController1 = require('/opt/render/project/src/controllers/slabController');
 
 router.get('/:year', slabController1.getSlabs);
 router.post('/', slabController1.saveSlabs);  
